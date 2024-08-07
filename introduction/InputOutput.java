eee212f47fb18d2784e1f106f40500e13bcc0a17
Dharshini R <116826746+dharshi1109@users.noreply.github.com>
2024-08-07 21:13:41 +0530

    Add files via upload

---
+++ introduction/InputOutput.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class InputOutput {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        int y = scan.nextInt();
+        int a = scan.nextInt();
+        int a1 = scan.nextInt();
+        System.out.println(y);
+        System.out.println(a);
+        System.out.println(a1);
+    }
+}
